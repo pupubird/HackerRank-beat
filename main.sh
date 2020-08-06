a3ced9e1d06672e8f8dd54647331c6c8eb24aa4f
pupubird <rainchai4240@gmail.com>
2020-08-06 18:54:28 +0800

    completed

--- main.sh
+++ main.sh
@@ -1,2 +1,11 @@
-read exp
-echo $exp | bc -l | xargs printf "%.3f"
+read n
+total=0
+
+# Add number to total in each loop
+for ((i = 0; i < $n ; i++))
+do
+    read num
+    total=$(( $total + $num ))
+done
+
+echo "$total / $n" | bc -l | xargs printf "%.3f"

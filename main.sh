1080dc37945dca00f0a5633d45c0baef427fd655
pupubird <rainchai4240@gmail.com>
2020-08-06 18:20:38 +0800

    completed

--- main.sh
+++ main.sh
@@ -1,13 +1,2 @@
-read x
-read y
-read z
-
-if [[ $x == $y && $y == $z ]]
-then
-    echo "EQUILATERAL"
-elif [[ $x == $y || $y == $z || $x == $z ]]
-then
-    echo "ISOSCELES"
-else
-    echo "SCALENE"
-fi
+read exp
+echo $exp | bc -l | xargs printf "%.3f"
